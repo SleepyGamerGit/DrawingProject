99e4b0d25c6bc13a50e72cbb01df80573a36fc9f
Sleepy <hillsteadm@gmail.com>
2018-11-01 09:01:00 -0600

    added screen and view

---
+++ DrawingProject/view/TableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.swift
+//  DrawingProject
+//
+//  Created by Hillstead, Matthew on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: NSObject {
+
+}
